ea2fab750edae55b53938196d5399239f2bb8371
Facundo <facundo.luzko@wyeworks.com>
2025-04-28 14:20:59 -0300

    add permissionlevel check for toolbar

--- src/components/spreadsheet.tsx
+++ src/components/spreadsheet.tsx
@@ -1,10 +1,12 @@
+import useSpreadsheetStore from "../lib/store";
 import { SpreadsheetGrid } from "./spreadsheet-grid";
 import { SpreadsheetToolbar } from "./spreadsheet-toolbar";
 
 export default function Spreadsheet() {
+  const { permissionLevel } = useSpreadsheetStore();
   return (
     <div className="relative flex flex-col gap-4">
-      <SpreadsheetToolbar />
+      {permissionLevel === "ld" && <SpreadsheetToolbar />}
       <SpreadsheetGrid />
     </div>
   );
