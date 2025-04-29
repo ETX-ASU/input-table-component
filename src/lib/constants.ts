7df4a32972c08e863dc9c7d720c69055ea6b80a1
Facundo <facundo.luzko@wyeworks.com>
2025-04-29 17:03:19 -0300

    add dark variations & replace constants with correct colors

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,16 +2,16 @@ import { WebSafeFont } from "../components/font-selector";
 import { TextAlign } from "./store";
 
 export const DEFAULT_BORDER_WIDTH = 0.5;
-export const DEFAULT_BORDER_COLOR = "#d1d5db";
+export const DEFAULT_BORDER_COLOR = "#d6d6d6";
 
 export const DEFAULT_BACKGROUND_COLOR = "transparent";
-export const BACKGROUND_COLOR_LIGHT_BLUE = "#e6f0ff";
-export const BACKGROUND_COLOR_LIGHT_GRAY = "#f5f5f5";
-export const BACKGROUND_COLOR_SUBTLE_GRAY = "#fafafa";
+export const BACKGROUND_COLOR_LIGHT_BLUE = "#cce5ee";
+export const BACKGROUND_COLOR_LIGHT_GRAY = "#ebebeb";
+export const BACKGROUND_COLOR_SUBTLE_GRAY = "#ebebeb";
 
 export const DEFAULT_FONT_FAMILY: WebSafeFont = WebSafeFont.Arial;
 export const DEFAULT_FONT_SIZE = 12;
-export const DEFAULT_FONT_COLOR = "#000000";
+export const DEFAULT_FONT_COLOR = "#333333";
 
 export const DEFAULT_ROW_HEIGHT = 28;
 export const DEFAULT_COLUMN_WIDTH = 100;
